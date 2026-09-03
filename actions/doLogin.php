b739914f29fdd189bd82472151a8ba5c340ee79d
Christopher L <40509915+christ0208@users.noreply.github.com>
2026-09-03 15:04:32 +0700

    fix(app): fix instructions

--- actions/doLogin.php
+++ actions/doLogin.php
@@ -4,7 +4,7 @@
 // Detail TODO:
 // 1. Get user credentials (email and password) that has been sent from login.php
 // 2. Compare sent user credentials and saved user credentials when register new user (refer to the Register page procedure and logic)
-// 3. If user comparation shows the user does not exists in saved user credentials, redirect back to login.php page and show error message of "Wrong username and password combination".
+// 3. If user comparation shows the user does not exists in saved user credentials, redirect back to login.php page and show error message of "Wrong user e-mail and password combination".
 // 4. If user comparation shows the user exists in saved user credentials, regenerate the session ID, save user data to session as logged on user and redirect to home.php
 
 // Notes:
